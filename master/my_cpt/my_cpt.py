d40b56893d94779d3b97f43ee4a2bc58531a7d57
carissatang <43019204+carissatang@users.noreply.github.com>
2019-01-13 23:19:00 -0500

    transition between pages
    // need to fix visuals and add functions

--- master/my_cpt/my_cpt.py
+++ master/my_cpt/my_cpt.py
@@ -1,94 +1,193 @@
 from Tkinter import *
 
-root = Tk()
-root.geometry("450x600")
-root.resizable(width=False, height=False)
-root.configure(background='snow')
 
-def login_page(create_account):
-    def login_label():
+class App(Tk):
+    def __init__(self, *args, **kwargs):
+        Tk.__init__(self, *args, **kwargs)
+
+        MainMenu(self)
+
+        container = Frame(self)
+        container.pack(side="top", fill="both", expand=True)
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
+
+        self.frames = {}
+
+        for F in (LoginPage, CreateAccount, CategoryPage, AddPage, ReportPage):
+            frame = F(container, self)
+            self.frames[F] = frame
+            frame.grid(row=0, column=0, sticky="nsew")
+            frame.config(background='snow')
+
+        self.show_frame(LoginPage)
+
+    def show_frame(self, context):
+        frame = self.frames[context]
+        frame.tkraise()
+
+
+class LoginPage(Frame):
+    def __init__(self, parent, controller):
+        Frame.__init__(self, parent)
+
         labelfont = ('helvetica', 50, 'bold')
-        login = Label(root, text="LOGIN")
+        login = Label(self, text="LOGIN")
         login.config(font=labelfont, relief=SUNKEN)
-        login.place(x=225, y=100, anchor=N)
+        login.grid(columnspan=2, pady=100, ipadx=10)
 
-
-    def user_label():
         labelfont = ('helvetica', 20)
-        user = Label(root, text="Username: ")
+        user = Label(self, text="Username: ", bg='snow')
         user.config(font=labelfont)
-        user.place(x=125, y=250, anchor=N)
-        user_entry = Entry(root)
-        user_entry.place(x=280, y=252, anchor=N)
+        user.grid(row=1, column=0, padx=(35, 10))
+        user_entry = Entry(self)
+        user_entry.grid(row=1, column=1, padx=(10, 60))
 
-
-    def pass_label():
-        labelfont = ('helvetica', 20)
-        password = Label(root, text="Password: ")
+        password = Label(self, text="Password: ", bg='snow')
         password.config(font=labelfont)
-        password.place(x=125, y=290, anchor=N)
-        password_entry = Entry(root)
-        password_entry.place(x=280, y=292, anchor=N)
+        password.grid(row=2, column=0, padx=(35, 10), pady=10)
+        password_entry = Entry(self)
+        password_entry.grid(row=2, column=1, padx=(10, 60), pady=10)
+
+        check = Checkbutton(self, text="Keep me logged in", bg='snow')
+        check.grid(row=3, columnspan=2, pady=10)
+
+        create_account = Button(self, text="Create a new account", command=lambda: controller.show_frame(CreateAccount))
+        create_account.grid(row=4, columnspan=2)
+
+        category_page = Button(self, text="Login", command=lambda: controller.show_frame(CategoryPage))
+        category_page.grid(row=5, columnspan=2, pady=(1, 120))
 
 
-    def buttons(create_account):
-        check = Checkbutton(root, text="Keep me logged in")
-        check.place(x=225, y=350, anchor=N)
+class CreateAccount(Frame):
+    def __init__(self, parent, controller):
+        Frame.__init__(self, parent)
 
-        button = Button(root, text="Create an account", command=create_account)
-        button.place(x=225, y=400, anchor=N)
+        labelfont = ('helvetica', 40, 'bold')
+        title = Label(self, text="CREATE ACCOUNT")
+        title.config(font=labelfont, relief=SUNKEN)
+        title.grid(columnspan=2, padx=20, pady=100, ipadx=10)
 
-
-    def main(create_account):
-        login_label()
-        user_label()
-        pass_label()
-        buttons(create_account)
-
-
-    main(create_account)
-
-def create_account():
-    def create_label():
-        labelfont = ('helvetica', 30, 'bold')
-        login_1 = Label(root, text="CREATE AN")
-        login_1.config(font=labelfont, relief=SUNKEN)
-        login_1.place(x=225, y=100, anchor=N)
-        login_2 = Label(root, text="ACCOUNT")
-        login_2.config(font=labelfont, relief=SUNKEN)
-        login_2.place(x=225, y=141, anchor=N)
-
-
-    def user_label():
         labelfont = ('helvetica', 20)
-        user = Label(root, text="Username: ")
+        user = Label(self, text="Username: ", bg='snow')
         user.config(font=labelfont)
-        user.place(x=125, y=250, anchor=N)
-        user_entry = Entry(root)
-        user_entry.place(x=280, y=252, anchor=N)
+        user.grid(row=1, column=0, padx=(35, 10))
+        user_entry = Entry(self)
+        user_entry.grid(row=1, column=1, padx=(10, 60))
 
-
-    def pass_label():
-        labelfont = ('helvetica', 20)
-        password = Label(root, text="Password: ")
+        password = Label(self, text="Password: ", bg='snow')
         password.config(font=labelfont)
-        password.place(x=125, y=290, anchor=N)
-        password_entry = Entry(root)
-        password_entry.place(x=280, y=292, anchor=N)
+        password.grid(row=2, column=0, padx=(35, 10), pady=10)
+        password_entry = Entry(self)
+        password_entry.grid(row=2, column=1, padx=(10, 60), pady=10)
+
+        category_page = Button(self, text="Create account", command=lambda: controller.show_frame(CategoryPage))
+        category_page.grid(row=3, columnspan=2, pady=10)
 
 
-    def create_button():
-        button = Button(root, text="Create")
-        button.place(x=225, y=350, anchor=N)
+class CategoryPage(Frame):
+    def __init__(self, parent, controller):
+        Frame.__init__(self, parent)
+
+        labelfont = ('helvetica', 50, 'bold')
+        title = Label(self, text="Category Page")
+        title.config(font=labelfont, relief=SUNKEN)
+        title.grid(columnspan=2, padx=10, pady=20, ipadx=10)
+
+        login_page = Button(self, text="Report", command=lambda: controller.show_frame(ReportPage))
+        login_page.grid(columnspan=2, row=7, pady=(20, 1))
+
+        labelfont = ('helvetica', 20)
+        technology = Button(self, text="technology", width=12, command=lambda: controller.show_frame(AddPage))
+        technology.config(font=labelfont)
+        technology.grid(pady=10, row=1, column=0)
+
+        clothing = Button(self, text="clothing", width=12, command=lambda: controller.show_frame(AddPage))
+        clothing.config(font=labelfont)
+        clothing.grid(pady=10, row=2, column=0)
+
+        shoes = Button(self, text="shoes", width=12, command=lambda: controller.show_frame(AddPage))
+        shoes.config(font=labelfont)
+        shoes.grid(pady=10, row=3, column=0)
+
+        public_transport = Button(self, text="public transport", width=12, command=lambda: controller.show_frame(AddPage))
+        public_transport.config(font=labelfont)
+        public_transport.grid(pady=10, row=4, column=0)
+
+        travel = Button(self, text="travel", width=12, command=lambda: controller.show_frame(AddPage))
+        travel.config(font=labelfont)
+        travel.grid(pady=10, row=5, column=0)
+
+        vehicle = Button(self, text="vehicle", width=12, command=lambda: controller.show_frame(AddPage))
+        vehicle.config(font=labelfont)
+        vehicle.grid(pady=10, row=6, column=0)
+
+        pet = Button(self, text="pet", width=12, command=lambda: controller.show_frame(AddPage))
+        pet.config(font=labelfont)
+        pet.grid(pady=10, row=1, column=1)
+
+        books = Button(self, text="books", width=12, command=lambda: controller.show_frame(AddPage))
+        books.config(font=labelfont)
+        books.grid(pady=10, row=2, column=1)
+
+        cosmetics = Button(self, text="cosmetics", width=12, command=lambda: controller.show_frame(AddPage))
+        cosmetics.config(font=labelfont)
+        cosmetics.grid(pady=10, row=3, column=1)
+
+        stationary = Button(self, text="stationary", width=12, command=lambda: controller.show_frame(AddPage))
+        stationary.config(font=labelfont)
+        stationary.grid(pady=10, row=4, column=1)
+
+        restaurant = Button(self, text="restaurant", width=12, command=lambda: controller.show_frame(AddPage))
+        restaurant.config(font=labelfont)
+        restaurant.grid(pady=10, row=5, column=1)
+
+        gifts = Button(self, text="gifts", width=12, command=lambda: controller.show_frame(AddPage))
+        gifts.config(font=labelfont)
+        gifts.grid(pady=10, row=6, column=1)
 
 
-    def main():
-        create_label()
-        user_label()
-        pass_label()
-        create_button()
+class AddPage(Frame):
+    def __init__(self, parent, controller):
+        Frame.__init__(self, parent)
+        labelfont = ('helvetica', 50, 'bold')
+        title = Label(self, text="'Category'")
+        title.config(font=labelfont, relief=SUNKEN)
+        title.pack(padx=10, pady=10)
 
-    main()
+        labelfont = ('helvetica', 20)
+        amount = Label(self, text="Amount: ")
+        amount.config(font=labelfont)
+        amount.pack(padx=10, pady=10)
+        amount_entry = Entry(self)
+        amount_entry.pack()
 
-login_page(create_account)
-root.mainloop()
+        add = Button(self, text="add", command=lambda: controller.show_frame(CategoryPage))
+        add.pack()
+
+
+class ReportPage(Frame):
+    def __init__(self, parent, controller):
+        Frame.__init__(self, parent)
+
+        labelfont = ('helvetica', 50, 'bold')
+        title = Label(self, text="Report")
+        title.config(font=labelfont, relief=SUNKEN)
+        title.pack(padx=10, pady=10)
+
+        add = Button(self, text="back", command=lambda: controller.show_frame(CategoryPage))
+        add.pack()
+
+
+
+class MainMenu:
+    def __init__(self, master):
+        menu_bar = Menu(master)
+        file_menu = Menu(menu_bar, tearoff=0)
+        file_menu.add_command(label="Exit", command=master.quit)
+        menu_bar.add_cascade(label="File", menu=file_menu)
+        master.config(menu=menu_bar)
+
+
+app = App()
+app.mainloop()
